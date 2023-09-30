e5d82b17eebfa668ee3513c2c7f7d76157919a84
Michel-Rooney <michelrooney16@gmail.com>
2023-09-30 00:57:24 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Start from "./screens/Start";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Start />
+    </>
+  );
+}
+
+export default App;
---
+++ src/screens/Start.jsx
@@ -0,0 +1,81 @@
+import "../styles/Start.css";
+
+function Start() {
+  return (
+    <>
+      <section className="Start-container">
+        <div className="Start-title-container">
+          <img
+            src="/src/assets/terminal.png"
+            alt="Terminal Simbolo"
+            className="Start-title-icon"
+          />
+          <div className="Start-title">
+            <h1 className="Start-title-h1">Michel Rooney</h1>
+            <h2 className="Start-title-h2">DESENVOLVEDOR</h2>
+          </div>
+        </div>
+        <div className="Start-social-container">
+          <a
+            href="https://github.com/Michel-Rooney/"
+            target="_blank"
+            rel="noreferrer"
+            className="Start-Social-item"
+          >
+            <img
+              src="/src/assets/technologies/github.png"
+              alt="GitHub Logo"
+              title="GitHub"
+              className="Start-Social-item-image"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/michel-rooney-aabb63234/"
+            target="_blank"
+            rel="noreferrer"
+            className="Start-Social-item"
+          >
+            <img
+              src="/src/assets/linkedin.png"
+              alt="Linkedin Logo"
+              title="LinkedIn"
+              className="Start-Social-item-image"
+            />
+          </a>
+          <a
+            href="/src/assets/Currículo-Michel-Rooney-2023.pdf"
+            download="Currículo-Michel-Rooney-2023"
+            target="_blank"
+            rel="noreferrer"
+            className="Start-Social-item"
+          >
+            <img
+              src="/src/assets/docs.png"
+              alt="Currículo Logo"
+              title="Currículo"
+              className="Start-Social-item-image"
+            />
+          </a>
+          <a
+            href="mailto:michelrooney16@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            className="Start-Social-item"
+          >
+            <img
+              src="/src/assets/gmail.png"
+              alt="Gmail Logo"
+              title="Gmail"
+              className="Start-Social-item-image"
+            />
+          </a>
+        </div>
+      </section>
+      <section className="Start-about-container">
+        
+      </section>
+    </>
+  );
+}
+
+export default Start;
